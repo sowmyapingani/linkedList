88713a53cd5b6395b751df9646ffd46df8cac255
manishamuplam@gmail.com <manishamulam@gmail.com>
2022-07-10 12:11:19 +0530

    added:adding

---
+++ linkedListMain.java
@@ -0,0 +1,15 @@
+package com.bridge.labzs;
+
+public class linkedListMain {
+    public static void main(String[] args) {
+    System.out.println("Welcome to linked list:");
+    LinkedList<E> linkedList = new LinkedList<E>();
+    linkedList.add(70);
+    linkedList.add(30);
+    linkedList.add(56);
+    System.out.println("After Adding the data : ");
+    linkedList.display();
+}
+}
+
+
---
+++ node.java
@@ -0,0 +1,17 @@
+package com.bridge.labzs;
+
+public class node {
+    public class Node<T> {
+        T data;
+        Node<T> next;
+
+        public class Node<T> {
+            T data;
+            Node<T> next;
+
+            public Node(T data) {
+                    this.data = (T) data;
+                }
+            }
+        }
+    }
